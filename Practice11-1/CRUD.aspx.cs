4b81d17597fedf1de7bc3fbfe1084026c1af549d
康恆 徐 <wa0125678665@>
2021-06-17 19:13:34 +0800

    done

--- Practice11-1/CRUD.aspx.cs
+++ Practice11-1/CRUD.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace Practice11_1
 {
@@ -11,7 +12,34 @@ namespace Practice11_1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string s_Conns = "";
+
+            s_Conns = "Data Source = (localdb)\\ProjectsV13;" +
+                " Initial Catalog = Test; Integrated Security = True; " +
+                "Connect Timeout = 30; Encrypt = False; " +
+                "TrustServerCertificate = False;" +
+                " ApplicationIntent = ReadWrite; MultiSubnetFailover = False;"+
+                "User ID = sa; Password = 12345";
+
+            SqlConnection o_Conn = new SqlConnection(s_Conns);
+            try
+            {
+                SqlCommand o_Com = new SqlCommand("Update Users set Birthday = '2021/6/17' where Name = N'狗狗旭'", o_Conn);
+                o_Conn.Open();
+                o_Com.ExecuteNonQuery();
+                o_Conn.Close();
+            }
+            catch(Exception o_Exc)
+            {
+                Response.Write(o_Exc.ToString());
+            }
+
+
+
+
 
         }
+
+
     }
 }
